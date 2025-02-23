8c71e50458c37e6e1e5a88d0ad0eb635635f73de
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2025-02-24 00:58:18 +0600

    header section

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatefulWidget{
+class Homescreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-   return  ExtendHomescreen();
+    return ExtendHomescreen();
   }
-
 }
 
-class ExtendHomescreen extends State<Homescreen>{
+class ExtendHomescreen extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.black54,
-     body:Container(
-       child: Text("hello"),
-     ),
-   );
-  }
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Align text to left
+                    children: [
+                      Text(
+                        "Hello Rafsan",
+                        style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Let's celebrate holiday",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    ],
+                  ),
 
+                  // Right Side: Logo or Icon
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/sports.png"), //
+                    radius: 24, //
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
